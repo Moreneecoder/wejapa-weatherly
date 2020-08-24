21585e627fa4c1a7af38d548f939582ac34bf93d
Morenikeji <bellokeji19@gmail.com>
2020-08-24 11:22:46 -1200

    gitignore and .env files added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const url = require('url')
 const processHtml = require('./app_modules/processHtml')
 const processXHR = require('./app_modules/processXHR');
+require('dotenv').config()
 
 
 
